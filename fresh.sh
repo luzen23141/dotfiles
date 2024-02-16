05b850657bf1727a30d83b3ff923f16d0006e444
alex <alex@truthidea.org>
2024-02-16 19:09:21 +0800

    tmp

--- fresh.sh
+++ fresh.sh
@@ -77,7 +77,7 @@ mackup -n restore
 echo "再視情況手動執行 mackup 指令"
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/dotfiles/iterm2"
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/dotfiles/appConfig/iterm2"
 
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
